043e4e83fad6280e6078094db25cd501fdb7f132
Derelict <oleksandr.dzyndzyura@gmail.com>
2021-08-04 09:02:57 +0300

    changes in the form of validation

--- 2/main.js
+++ 2/main.js
@@ -1,57 +1,87 @@
-function validationEmail() {
-    var form = document.getElementById("form")
-    var email = document.getElementById("email").value;
-    var text = document.getElementById("text")
-    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+'use strict'
 
-    if(email.match(pattern)) {
-        form.classList.add("valid")
-        form.classList.remove("invalid")
-        text.innerHTML = "You Email Address in Valid"
-        text.style.color = "#00ff00"
-    }
+const PASSWORD_REQUIRED = "Please enter your Pass";
+const PASSWORD_INVALID = "Please Enter Valid Pass";
+const EMAIL_REQUIRED = "Please enter your email";
+const EMAIL_INVALID = "Please enter valid email";
 
-    else {
 
-        form.classList.remove("valid")
-        form.classList.add("valid")
-        text.innerHTML = "Please Enter Valid Email"
-        text.style.color = "#ff0000"
-    }
+const form = document.querySelector("#signin");
 
-    if( email == "" ) {
-        form.classList.remove("valid")
-        form.classList.remove("invalid")
-        text.innerHTML = ""
-        text.style.color = "#ff0000"
-    }
+const showMessage = (el, message, type) => {
+	el.innerHTML = message;
+	el.style.color = type ? "#00ff00" : "#ff0000";
+	return type;
 }
 
-function validationPass() {
-    var passForm = document.getElementById("passForm")
-    var password = document.getElementById("password").value;
-    var textPass = document.getElementById("textPass")
-    var pattern = /^.{6,}$/;
+const showError = (el, message) => {
+	return showMessage(el, message, false);
+}
 
-    if(password.match(pattern)) {
-        passForm.classList.add("valid")
-        passForm.classList.remove("invalid")
-        textPass.innerHTML = "You Email Password in Valid"
-        textPass.style.color = "#00ff00"
-    }
+const showSuccess = el => {
+	return showMessage(el, "", true);
+}
 
-    else {
+const hasValue = (input, message, wrap) => {
+	if (input.trim() === "") {
+		return showError(wrap, message);
+	}
+	return showSuccess(wrap);		
+}	
 
-        passForm.classList.remove("valid")
-        passForm.classList.add("valid")
-        textPass.innerHTML = "Please Enter Valid Pass"
-        textPass.style.color = "#ff0000"
-    }
+form.addEventListener("submit", function (event) {
+	event.preventDefault();
 
-    if( password == "" ) {
-        passForm.classList.remove("valid")
-        passForm.classList.remove("invalid")
-        textPass.innerHTML = ""
-        textPass.style.color = "#ff0000"
-    }
+	let passValid = validationPass(form.elements["password"], PASSWORD_REQUIRED, PASSWORD_INVALID);
+	let emailValid = validationEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+
+	if (passValid && emailValid) {
+		alert("Submit Form");
+	}
+});
+
+const validationEmail = (el, requiredMsg, invalidMsg) => {
+    const form = document.getElementById("form");
+    const email = document.getElementById("email").value;
+    const text = document.getElementById("text");
+    const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (!hasValue(email, requiredMsg, text)) {
+    	form.classList.remove("valid");
+        form.classList.add("invalid");
+		return false;
+	}
+
+	if (!email.match(pattern)) {
+		form.classList.remove("valid");
+        form.classList.add("invalid");
+		return showError(text, invalidMsg);
+	}	
+ 	form.classList.remove("valid");
+ 	form.classList.remove("invalid");
+
+	return true;
+}
+
+const validationPass = (el, requiredMsg, invalidMsg) => {
+    const passForm = document.getElementById("passForm")
+    const password = document.getElementById("password").value;
+    const textPass = document.getElementById("textPass")
+    const pattern = /^.{6,}$/;
+
+    if (!hasValue(password, requiredMsg, textPass)) {
+    	passForm.classList.remove("valid");
+        passForm.classList.add("invalid");
+		return false;
+	}
+
+	if (!password.match(pattern)) {
+		passForm.classList.remove("valid");
+        passForm.classList.add("invalid");
+		return showError(textPass, invalidMsg);
+	}
+ 	passForm.classList.remove("valid");
+ 	passForm.classList.remove("invalid");
+
+	return true;
 }
